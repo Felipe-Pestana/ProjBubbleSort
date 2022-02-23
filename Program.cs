34131116251cfcdff729d800f3f6f2c5b8a66acf
Felipe Pestana <pestana@sitic.com.br>
2022-02-23 09:53:15 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ProjBubbleSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lista = new string[5];
+
+            for(int i = 0; i < 5; i++)pos\ProjBubbleSort
+            {
+                lista[i] = Console.ReadLine();
+            }
+
+            for(int i = 0; i < 5; i++)
+                Console.Write(lista[i]+" ");
+
+            for (int referencia = 0; referencia < 5; referencia++)
+            {
+                for(int comparacao = referencia+1; comparacao < 5; comparacao++)
+                {
+                    if (String.Compare(lista[referencia], lista[comparacao]) < 0)
+                    {
+                        string aux = lista[referencia];
+                        lista[referencia] = lista[comparacao];
+                        lista[comparacao] = aux;
+                    }
+                }
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < 5; i++)
+                Console.Write(lista[i] + " ");
+        }
+    }
+}
